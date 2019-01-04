f4bbf052f9f5802c0f55b94dbca85a5fb4966c8e
Emerson Barros <kaua54@hotmail.com>
2019-01-04 14:47:53 -0200

    Projeto criado

---
+++ src/main/java/br/com/barrostech/SpringBootMongodbApplication.java
@@ -0,0 +1,14 @@
+package br.com.barrostech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbApplication.class, args);
+	}
+
+}
+
